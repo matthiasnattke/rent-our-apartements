c0cdc57cea50e2b8d54c5881577789a073176690
Matthias Nattke <matthias@couch-blog.de>
2014-12-20 17:02:33 +0100

    Footer repariert, Bilder aufgeschönt

--- js/init.js
+++ js/init.js
@@ -14,45 +14,38 @@
 		var	$window = $(window),
 			$body = $('body'),
 			$header = $('#header'),
-			$all = $body.add($header),
 			sectionTransitionState = false;
-
-		
-
-			// Resize.
-				var resizeTimeout, resizeScrollTimeout;
-				
-				$window.resize(function() {
-
-					window.clearTimeout(resizeTimeout);
-
-					resizeTimeout = window.setTimeout(function() {
-
-						// Resize fullscreen elements.
+        
+        // Resize our element.
+        var resizeTimeout, resizeScrollTimeout;
+            
+            $window.resize(function() {
+                
+                window.clearTimeout(resizeTimeout);
+                
+                    resizeTimeout = window.setTimeout(function() {
+                        
+                        // Resize fullscreen elements.
 							if (settings.fullScreen) {
+                                
 								$('.fullscreen').each(function() {
 								
 									var $t = $(this),
-										$c = $t.children('main'),
-										x = Math.max(100, Math.round(($window.height()) / 2) - 90);
+										x = Math.max(100, Math.round(($window.height())) - 180);
 
-									$t
-										.css('padding-top', x)
-										.css('padding-bottom', x);
-								
+									$t.css('min-height', x);
 								});
 							}
 					}, 100);
 
-				});
+            });
 				
 		// Trigger resize
-			$window.load(function() {
-				
-				$window
-					.trigger('resize')
-			
-			});
+        $window.load(function() {
+            
+            $window
+                .trigger('resize')	
+        });
 
 	});
 	
